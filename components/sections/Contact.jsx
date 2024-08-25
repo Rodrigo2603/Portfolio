a4b9228bbc0a6e1bf2f9393b5c4084810cc18a26
Rodrigo Rocha <78671937+Rodrigo2603@users.noreply.github.com>
2024-08-24 22:13:22 -0300

    Update Contact.jsx

--- components/sections/Contact.jsx
+++ components/sections/Contact.jsx
@@ -7,9 +7,9 @@ import SectionContainer from "../SectionContainer";
 
 export default function Contact() {
   return (
-    <SectionContainer id="contato" name="contato" headerText="Como entrar em contato?" justifyContent="center" alignItems="center useHeaderStyle>
-      <Flex as={Grid} container item xs={9} sm={6} pl={4} pr={4}>
-        <Text fontSize="xl" mt={6}>
+    <SectionContainer id="contato" name="contato" headerText="Como entrar em contato?"  useHeaderStyle>
+      <Flex as={Grid} container item xs={9} sm={6} justify="center" textAlign="center" pl={4} pr={4}>
+        <Text fontSize="xl" m="auto" mt={6}>
           Por favor, sinta-se à vontade para me enviar um e-mail caso tenha qualquer dúvida ou necessite de mais informações.
         </Text>
         <Button as={Link} leftIcon={<EmailIcon />} href="mailto:rodrigorocha1032@gmail.com" m="auto" mt={6}>
