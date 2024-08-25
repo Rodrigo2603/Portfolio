c4841e3d2390fcf81a70f70a2dd41b084f308854
Rodrigo Rocha <78671937+Rodrigo2603@users.noreply.github.com>
2024-08-25 12:46:59 -0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
 
-import Script from 'next/script';
 import NavBar from "../components/NavBar";
 import theme from "../theme";
 
@@ -21,15 +20,6 @@ function App({ Component, pageProps }) {
   const pathToTitle = {
     "/": "Rodrigo Rocha - Desenvolvedor Full Stack",
   };
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DT8FV1T0WD"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-DT8FV1T0WD');
-</script>
 
   return (
     <ChakraProvider theme={theme}>
